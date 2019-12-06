72bd65fc7d17953285508b8f2e493f98248e2916
John Willes <johnwilles@gmail.com>
2019-12-05 19:24:44 -0500

    Working on kalman gain

--- src/core/kf.py
+++ src/core/kf.py
@@ -4,10 +4,8 @@ class KalmanFilter():
 
     def __init__(self):
         self.frame_rate = 0.1
-
-        #self.x = None
-        #self.P = np.eye(11) * 0.1
-
+ 
+        # Motion
         self.A = np.eye(11)
         self.A[0, 7] = self.frame_rate
         self.A[1, 8] = self.frame_rate
@@ -16,31 +14,21 @@ class KalmanFilter():
 
         self.Q = np.eye(11) * 0.1
 
-    # def update(self, state, covariance, detections):
-    #     self.x = state
-    #     self.P = covariance
+        # Observation
+        self.C = np.eye(7)
+        self.C = np.concatentate(self.C, np.zeros((7, 4)))
 
-    #     self.update_prediction()
-    #     self.update_kalman_gain()
-    #     self.update_correction()
-
-    #     return self.x, self.P
+        self.R = np.eye(7) * 0.1
 
     def update_prediction(self, x, P):
-        #x = np.matlmul(self.A, x)
         x = self.A @ x
         P = self.A @ P @ np.transpose(self.A) + self.Q
         return x, P
 
-    def update_kalman_gain(self):
-        pass
+    def update_correction(self, x, P, detection):
+        K = P @ np.transpose(self.C) @ np.linalg.inv()
+        
+         @ P @ np.transpose(self.G) + self.R
 
-    def update_correction(self):
-        pass
-
-    def update_motion(self, state):
-        return np.matmul(self.A, state)
-
-    def observation(self):
-        pass
+        return x, P
 
